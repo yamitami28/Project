602c2f01a4d28db78809b59d47babab154aeeb90
yamitami28 <yamila.johann@gmail.com>
2021-08-11 21:28:16 -0300

    entrega1

---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded",function(e){
+
+    document.getElementById("button").onclick = 
+    location.href= './C:\Users\docente\Desktop\Workspace+inicial';
+});
