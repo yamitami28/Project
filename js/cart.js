a4141a96614cfc40d9d7721b2f330a2cb1ae3ff5
yamitami28 <yamila.johann@gmail.com>
2021-11-01 20:21:37 -0300

    revisar

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,54 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+let productCount = document.getElementById("productCountInput");
+let productCost = 100;
+let comissionPercentage = 0.15;
+let MONEY_SYMBOL = "$";
+let DOLLAR_CURRENCY = "Dólares (USD)";
+let PESO_CURRENCY = "Pesos Uruguayos (UYU)";
+let DOLLAR_SYMBOL = "USD ";
+let PESO_SYMBOL = "UYU ";
+let PERCENTAGE_SYMBOL = '%';
+let SUCCESS_MSG = "¡Se ha realizado la publicación con éxito! :)";
+let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
+
+function updateTotalCosts(){
+ 
+  let comissionCostHTML = document.getElementById("comissionText");
+  let totalCostHTML = document.getElementById("totalCostText");
+  let subtotal = productCost*productCount
+  let unitCostToShow = MONEY_SYMBOL + productCost;
+  let comissionToShow = Math.round((comissionPercentage * 100));
+  let totalCostToShow = MONEY_SYMBOL + (Math.round(productCost * comissionPercentage * 100) / 100 + subtotal) ;
+
+  productCost.innerHTML = unitCostToShow;
+  comissionCostHTML.innerHTML = comissionToShow;
+  totalCostHTML.innerHTML = totalCostToShow;
+}
+
 document.addEventListener("DOMContentLoaded", function(e){
 
     function showProductCart(array){ // preguntar por el a
 
         let HTMLCarrito = "";
-            let carrito = array.articles[0];
+
+          let carrito = array.articles[0];
+          
+          productCost = carrito.unitCost;  
+          
+          
+          function updateTotalCosts(){
+ 
+            let comissionCostHTML = document.getElementById("comissionText");
+            let totalCostHTML = document.getElementById("totalCostText");
+            let subtotal = productCost*productCount
+            let unitCostToShow = MONEY_SYMBOL + productCost;
+            let comissionToShow = Math.round((comissionPercentage * 100));
+            let totalCostToShow = MONEY_SYMBOL + (Math.round(productCost * comissionPercentage * 100) / 100 + subtotal) ;
+          
+            productCost.innerHTML = unitCostToShow;
+            comissionCostHTML.innerHTML = comissionToShow;
+            totalCostHTML.innerHTML = totalCostToShow;
+          }
+           
   
             HTMLCarrito += `
 
@@ -40,13 +82,14 @@ document.addEventListener("DOMContentLoaded", function(e){
                     <div class="col-md-3 mb-3">
                       <label for="zip">Precio</label>
 
-                    <br><span class = "cuadrado">  ` + carrito.unitCost + ` <\span>
+                   
+                      <br><span class = "cuadrado">  ` + carrito.unitCost + ` <\span>
 
                     </div>
                     <div class="col-md-4 mb-3">
                         <label for="state">Moneda</label>
                         
-                    <br><span class = "cuadrado">    ` + carrito.currency + `<\span>
+                    <br><span class = "cuadrado"> ` + carrito.currency + `<\span>
                       </div>
                   </div>
 
@@ -60,7 +103,24 @@ document.addEventListener("DOMContentLoaded", function(e){
                       </div>
                     </div>
                   <hr class="mb-4">
-    
+                  <h5 class="mb-3">Tipo de publicación</h5>
+                      <div class="d-block my-3">
+                        <div class="custom-control custom-radio">
+                          <input id="premiumradio" name="publicationType" type="radio" class="custom-control-input" checked="" required="">
+                          <label class="custom-control-label" for="premiumradio">Premium (15%)</label>
+                        </div>
+                        <div class="custom-control custom-radio">
+                          <input id="expressradio" name="publicationType" type="radio" class="custom-control-input" required="">
+                          <label class="custom-control-label" for="expressradio">Express (7%)</label>
+                        </div>
+                        <div class="custom-control custom-radio">
+                          <input id="standardradio" name="publicationType" type="radio" class="custom-control-input" required="">
+                          <label class="custom-control-label" for="standardradio">Stándar (5%)</label>
+                        </div>
+                        <div class="row">
+                          <button type="button" class="m-1 btn btn-link" data-toggle="modal" data-target="#contidionsModal">Ver condiciones</button>
+                        </div>
+                      </div>
                  
                   <hr class="mb-4">
                   <h4 class="mb-3">Costos</h4>
@@ -70,7 +130,7 @@ document.addEventListener("DOMContentLoaded", function(e){
                           <h6 class="my-0">Subtotal</h6>
                           <small class="text-muted">Unitario del producto</small>
                         </div>
-                        <span class="text-muted" id="productCostText">` + carrito.unitCost * carrito.count+ ` </span>
+                        <span class="text-muted" id="productCostText">` + carrito.unitCost * productCount+ ` </span>
                       </li>
                       <li class="list-group-item d-flex justify-content-between lh-condensed">
                         <div>
@@ -94,20 +154,40 @@ document.addEventListener("DOMContentLoaded", function(e){
 
             `
             document.getElementById("productoCarrito").innerHTML = HTMLCarrito;
-            document.getElementById("productCountInput").value = carrito.count;
+            
+            document.getElementById("productCountInput").addEventListener("change", function(){
+              productCount = this.value;
+              updateTotalCosts();
+            });
 
+            document.getElementById("premiumradio").addEventListener("change", function(){
+              comissionPercentage = 0.13;
+              updateTotalCosts();
+          });
+          
+          document.getElementById("expressradio").addEventListener("change", function(){
+              comissionPercentage = 0.07;
+              updateTotalCosts();
+          });
+      
+          document.getElementById("standardradio").addEventListener("change", function(){
+              comissionPercentage = 0.03;
+              updateTotalCosts();
+          });
 
-    }
+          }
 
-
-    getJSONData(CART_INFO_URL).then(function(resultObj){ // resultObj es un nombre habitual
-        if (resultObj.status === "ok") 
-        {
-            productoCarrito = resultObj.data;
-            //Muestro las categorías ordenadas
-            showProductCart(productoCarrito);
-        }
-
-    });
-
+          getJSONData(CART_INFO_URL).then(function(resultObj){ // resultObj es un nombre habitual
+            if (resultObj.status === "ok") 
+            {
+                productoCarrito = resultObj.data;
+                //Muestro las categorías ordenadas
+                showProductCart(productoCarrito);
+          
+                console.log(document.getElementById("productCountInput").value)
+            }
+          
+          });
+   
 });
+
